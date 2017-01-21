15a72957d9f550df8d84fb467fd8d02ba5b3a14a
Sora Arakawa <info@arkw.net>
2017-01-21 19:07:06 +0900

    Update 2017/1/21

---
+++ katamuki.js
@@ -0,0 +1,12 @@
+/*
+Katamuki.js Ver.1.0
+(c)2015-2017 Sora Arakawa all rights reserved.
+*/
+
+window.onload=function(){
+	if(window.navigator.userAgent.toLowerCase().indexOf("chrome")!=-1){
+		document.body.style.transform="rotate(0.05deg)";
+    }else{
+		document.body.style.transform="rotate(0.001deg)";
+	}
+}
