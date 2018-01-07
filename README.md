df5d5832de0beda54aefaf325a3a761f4f1d8cf0
Sora Arakawa <sr@arkw.jp>
2018-01-08 07:37:12 +0900

    Update 2018/1/8

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 body要素を僅かに傾けることでブラウザが自動的にアンチエイリアス処理を実行する仕組みを利用しています。
 
 ## 使い方
-katamuki.jsをscriptタグでhead要素内に記述して下さい。
+katamuki.js または katamuki.min.jsをscriptタグでhead要素内に記述して下さい。
 
 ウェブサイトの読み込み時にアンチエイリアス処理が自動で行われます。
 
     <script type="text/javascript" src="katamuki.js"></script>
 
 ## katamuki.min.jsについて
-ソースコード圧縮に失敗していた為、廃止しました (2017年2月11日)
+~~ソースコード圧縮に失敗していた為、廃止しました (2017年2月11日)~~
+
+Ver.1.1公開に伴い、復活しました。(2018年1月8日)
--- katamuki.js
+++ katamuki.js
@@ -1,12 +1,20 @@
 /*
-Katamuki.js Ver.1.0
-(c)2015-2017 Sora Arakawa all rights reserved.
+Katamuki.js Ver.1.1
+(c)2015-2018 Sora Arakawa all rights reserved.
 */
 
 window.onload=function(){
+	var image_list = document.getElementsByTagName("img");
 	if(window.navigator.userAgent.toLowerCase().indexOf("chrome")!=-1){
 		document.body.style.transform="rotate(0.05deg)";
+		document.getElementsByTagName("img").style.transform="rotate(-0.05deg)";
+		for (var i=0; i<=image_list.length; i++){
+			image_list[i].style.transform="rotate(-0.05deg)";
+		}
 	}else{
 		document.body.style.transform="rotate(0.001deg)";
+		for (var i=0; i<=image_list.length; i++){
+			image_list[i].style.transform="rotate(-0.001deg)";
+		}
 	}
 }
